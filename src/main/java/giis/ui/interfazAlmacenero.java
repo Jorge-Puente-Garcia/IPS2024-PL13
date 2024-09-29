6324b7800f2d43146bd9ac881d4dd6a6567d8c88
Jorge Puente García <Uo294228@uniovi.es>
2024-09-29 14:48:18 +0200

    Finalización de la bbdd y su inicialización.

---
+++ src/main/java/giis/ui/interfazAlmacenero.java
@@ -0,0 +1,115 @@
+package giis.ui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import java.awt.CardLayout;
+import javax.swing.JPanel;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JScrollPane;
+import java.awt.FlowLayout;
+import javax.swing.BoxLayout;
+import java.awt.Color;
+
+public class interfazAlmacenero {
+
+	private JFrame frameTerminalPortatil;
+	private JPanel pnPaginaPrincipal;
+	private JButton btnVerOrdenesDeTrabajo;
+	private JPanel pnOrdenesDeTrabajo;
+	private JButton btnVolverAPaginaPrincipal;
+	private JScrollPane scrollPane;
+	private JPanel pnVisualizaciónOrdenesDeTrabajo;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					interfazAlmacenero window = new interfazAlmacenero();
+					window.frameTerminalPortatil.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public interfazAlmacenero() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frameTerminalPortatil = new JFrame();
+		frameTerminalPortatil.setBounds(100, 100, 698, 528);
+		frameTerminalPortatil.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frameTerminalPortatil.getContentPane().setLayout(new CardLayout(0, 0));
+		frameTerminalPortatil.getContentPane().add(getPnPaginaPrincipal(), "pnPaginaPrincipal");
+		frameTerminalPortatil.getContentPane().add(getPnOrdenesDeTrabajo(), "pnOrdenesDeTrabajo");
+	}
+
+	private JPanel getPnPaginaPrincipal() {
+		if (pnPaginaPrincipal == null) {
+			pnPaginaPrincipal = new JPanel();
+			pnPaginaPrincipal.setLayout(new GridLayout(3, 1, 0, 0));
+			pnPaginaPrincipal.add(getBtnVerOrdenesDeTrabajo());
+		}
+		return pnPaginaPrincipal;
+	}
+	private JButton getBtnVerOrdenesDeTrabajo() {
+		if (btnVerOrdenesDeTrabajo == null) {
+			btnVerOrdenesDeTrabajo = new JButton("Ordenes de trbajo");
+			btnVerOrdenesDeTrabajo.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					CardLayout cl = (CardLayout)(frameTerminalPortatil.getContentPane().getLayout());
+					cl.show(frameTerminalPortatil.getContentPane(), "pnOrdenesDeTrabajo");
+				}
+			});
+		}
+		return btnVerOrdenesDeTrabajo;
+	}
+	private JPanel getPnOrdenesDeTrabajo() {
+		if (pnOrdenesDeTrabajo == null) {
+			pnOrdenesDeTrabajo = new JPanel();
+			pnOrdenesDeTrabajo.setLayout(null);
+			pnOrdenesDeTrabajo.add(getBtnVolverAPaginaPrincipal());
+			pnOrdenesDeTrabajo.add(getScrollPane());
+		}
+		return pnOrdenesDeTrabajo;
+	}
+	private JButton getBtnVolverAPaginaPrincipal() {
+		if (btnVolverAPaginaPrincipal == null) {
+			btnVolverAPaginaPrincipal = new JButton("Volver");
+			btnVolverAPaginaPrincipal.setEnabled(true);
+			btnVolverAPaginaPrincipal.setBounds(10, 10, 110, 34);
+		}
+		return btnVolverAPaginaPrincipal;
+	}
+	private JScrollPane getScrollPane() {
+		if (scrollPane == null) {
+			scrollPane = new JScrollPane();
+			scrollPane.setBounds(10, 54, 664, 427);
+			scrollPane.setViewportView(getPanel_2());
+		}
+		return scrollPane;
+	}
+	private JPanel getPanel_2() {
+		if (pnVisualizaciónOrdenesDeTrabajo == null) {
+			pnVisualizaciónOrdenesDeTrabajo = new JPanel();
+			pnVisualizaciónOrdenesDeTrabajo.setBackground(Color.LIGHT_GRAY);
+			pnVisualizaciónOrdenesDeTrabajo.setLayout(new BoxLayout(pnVisualizaciónOrdenesDeTrabajo, BoxLayout.Y_AXIS));
+		}
+		return pnVisualizaciónOrdenesDeTrabajo;
+	}
+}
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,14 +1,24 @@
---Primero se deben borrar todas las tablas (de detalle a maestro) y lugo anyadirlas (de maestro a detalle)
---(en este caso en cada aplicacion se usa solo una tabla, por lo que no hace falta)
-
---Para giis.demo.tkrun:
-drop table Carreras;
 drop table Producto;
 drop table Cliente;
 drop table PedidoProducto;
-drop table ;
+drop table Pedido;
+drop table Carrito;
+drop table CarritoProducto;
+drop table Almacenero;
+drop table OrdenTrabajo;
+drop table OrdenTrabajoProducto;
+drop table Incidencia;
 
-create table Carreras (id int primary key not null, inicio date not null, fin date not null, fecha date not null, descr varchar(32), check(inicio<=fin), check(fin<fecha));
-create table Producto (id int PRIMARY KEY, TEXT NOT NULL, datosBasicos TEXT NOT NULL); 
-CREATE TABLE Cliente (dni TEXT PRIMARY KEY, nombre TEXT NOT NULL, apellidos TEXT NOT NULL);
-CREATE TABLE PedidoProducto (id INTEGER PRIMARY KEY, cliente_dni TEXT NOT NULL, producto_id INTEGER NOT NULL, cantidad INTEGER NOT NULL, precio DECIMAL(10, 2) NOT NULL, FOREIGN KEY (cliente_dni) REFERENCES Cliente(dni), FOREIGN KEY (producto_id) REFERENCES Producto(id));
+CREATE TABLE Producto (id int PRIMARY KEY, referencia TEXT NOT NULL, datosBasicos TEXT NOT NULL); 
+CREATE TABLE Cliente (id TEXT PRIMARY KEY,dni TEXT NOT NULL, nombre TEXT NOT NULL, apellidos TEXT NOT NULL, UNIQUE(dni));
+CREATE TABLE PedidoProducto (id INTEGER PRIMARY KEY, pedido_id INTEGER NOT NULL, producto_id INTEGER NOT NULL, cantidad INTEGER NOT NULL, precio DECIMAL(10, 2) NOT NULL, FOREIGN KEY (pedido_id) REFERENCES Pedido(id), FOREIGN KEY (producto_id) REFERENCES Producto(id));
+CREATE TABLE Pedido (id INTEGER PRIMARY KEY, cliente_dni TEXT NOT NULL, fecha DATE NOT NULL, total DECIMAL(10, 2) NOT NULL, estado TEXT NOT NULL, orden_trabajo_id INTEGER, FOREIGN KEY (cliente_dni) REFERENCES Cliente(dni), FOREIGN KEY (orden_trabajo_id) REFERENCES OrdenTrabajo(id));
+CREATE TABLE Carrito (id INTEGER PRIMARY KEY, cliente_dni TEXT NOT NULL, fecha_creacion DATE NOT NULL, FOREIGN KEY (cliente_dni) REFERENCES Cliente(dni));
+CREATE TABLE CarritoProducto (id INTEGER PRIMARY KEY, carrito_id INTEGER NOT NULL, producto_id INTEGER NOT NULL, cantidad INTEGER NOT NULL, FOREIGN KEY (carrito_id) REFERENCES Carrito(id), FOREIGN KEY (producto_id) REFERENCES Producto(id));
+CREATE TABLE Almacenero (id INTEGER PRIMARY KEY, nombre TEXT NOT NULL, apellido TEXT NOT NULL);
+CREATE TABLE OrdenTrabajo (id INTEGER PRIMARY KEY, fecha_creacion DATE NOT NULL, estado TEXT NOT NULL, almacenero_id INTEGER NOT NULL, incidencia TEXT, FOREIGN KEY (almacenero_id) REFERENCES Almacenero(id));
+CREATE TABLE OrdenTrabajoProducto (id INTEGER PRIMARY KEY, orden_trabajo_id INTEGER NOT NULL, producto_id INTEGER NOT NULL, cantidad INTEGER NOT NULL, FOREIGN KEY (orden_trabajo_id) REFERENCES OrdenTrabajo(id), FOREIGN KEY (producto_id) REFERENCES Producto(id));
+CREATE TABLE Incidencia (id INTEGER PRIMARY KEY, orden_trabajo_id INTEGER NOT NULL, descripcion TEXT NOT NULL, fecha DATE NOT NULL, FOREIGN KEY (orden_trabajo_id) REFERENCES OrdenTrabajo(id));
+
+
+
