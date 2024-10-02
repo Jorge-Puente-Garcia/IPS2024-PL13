55293ecf55f3123dad40ee57049d7bc77ee89582
Usuario <Usuario@LAPTOP-TASIUUVD>
2024-10-02 11:17:23 +0200

    Orden trabajo

---
+++ src/main/java/giis/model/Cliente.java
@@ -0,0 +1,38 @@
+package giis.model;
+
+public class Cliente {
+    private String dni;
+    private String nombre;
+    private String apellidos;
+
+    public Cliente(String dni, String nombre, String apellidos) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+    }
+
+    // Getters y Setters
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+}
